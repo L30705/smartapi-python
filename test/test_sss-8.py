210539b27e280bec6f1e690e045b8b412e70de4b
rimagiri <126047113+rimagiri@users.noreply.github.com>
2023-07-06 14:43:40 +0530

    Add files via upload

---
+++ test/test_sss-8.py
@@ -0,0 +1,42 @@
+from SmartApi.smartConnect import SmartConnect
+import pyotp
+
+api_key = 'api-key'
+username = 'client-code'
+pwd = 'pin'
+smartApi = SmartConnect(api_key)
+token = "QR-code value"
+totp=pyotp.TOTP(token).now()
+correlation_id = "abcde"
+data = smartApi.generateSession(username, pwd, totp)
+# print(data)
+authToken = data['data']['jwtToken']
+refreshToken = data['data']['refreshToken']
+feedToken = smartApi.getfeedToken()
+# print("Feed-Token :", feedToken)
+res = smartApi.getProfile(refreshToken)
+# print("Res:", res)
+smartApi.generateToken(refreshToken)
+res=res['data']['exchanges']
+
+try:
+    orderparams = { "variety": "NORMAL",
+                    "tradingsymbol": "RELIANCE-EQ",
+                    "symboltoken": "122334875538",
+                    "transactiontype": "BUY",
+                    "exchange": "NSE",
+                    "ordertype": "LIMIT",
+                    "producttype": "DELIVERY",
+                    "duration": "DAY",
+                    "price": "2430",
+                    "squareoff": "0",
+                    "stoploss": "0",
+                    "quantity": "8"
+                    }
+    orderId=smartApi.placeOrder(orderparams)
+    print("The order id is: {}".format(orderId))
+except Exception as e:
+    if e.message == "Couldn't parse the JSON response received from the server":
+        print("Invalid symboltoken. Please check the symboltoken value.")
+    else:
+        print("Order placement failed: {}".format(e.message))
